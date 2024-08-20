1a883784f6a80a4e0a853e4eaf4a08c2b48e0db5
A2BOO <khayaiwong_rew@hotmail.com>
2024-08-20 18:00:56 +0700

    Add => lastest

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1></h1>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hellow World</h1>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
   </div>
 </template>
